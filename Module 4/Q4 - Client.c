60f1c18c84c7b300ba81734de683beaedf26dc41
pkanagashanmugam <145273761+pkanagashanmugam@users.noreply.github.com>
2025-05-24 11:58:22 +0530

    Create Q4 - Client.c

---
+++ Module 4/Q4 - Client.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+#define PORT 65500
+#define BUFFER_SIZE 1024
+
+int main(){
+        struct sockaddr_in server_address;
+        char buffer[BUFFER_SIZE];
+        char *message = "Hello from Client!";
+        memset(buffer, 0, BUFFER_SIZE);
+        int sock = socket(AF_INET,SOCK_STREAM,0);
+        if( sock < 0){
+                perror("Socket Creation Failed");
+                return 1;
+        }
+        server_address.sin_family = AF_INET;
+        server_address.sin_port = htons(PORT);
+
+        if(inet_pton(AF_INET,"127.0.0.1",&server_address.sin_addr) <= 0){
+                perror("Address not supported");
+                return 2;
+        }
+        if(connect(sock,(struct sockaddr *)&server_address,sizeof(server_address)) <0){
+                perror("Connection to Server Failed!");
+                return 3;
+        }
+        send(sock,message,strlen(message),0);
+        printf("Hello Message sent to server\n");
+
+        read(sock,buffer,BUFFER_SIZE);
+        printf("Message from Server : %s",buffer);
+
+        close(sock);
+
+        return 0;
+}
